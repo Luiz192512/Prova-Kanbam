0eb9ca0fc11a3235957ec6de546da79ce6e764f0
Luiz192512 <enz.luizgustavo@gmail.com>
2024-04-15 22:57:54 -0300

    gay

--- Kanban 4.0/js/SignUp.js
+++ Kanban 4.0/js/SignUp.js
@@ -1,11 +1,14 @@
-var users = [];
+  // Função para lidar com o cadastro
+  function signUp() {
+    var username = document.getElementById('signupUsername').value;
+    var password = document.getElementById('signupPassword').value;
+    localStorage.setItem(username, password);
+    alert('Cadastro realizado com sucesso!');
+    document.getElementById('signupForm').reset();
+  }
 
-// Function to sign up
-function signup() {
-  var email = document.getElementById("signup-email").value;
-  var password = document.getElementById("signup-password").value;
-
-  users.push({ email: email, password: password });
-
-  alert("Sign up successful!");
-}
+  // Adiciona o event listener para o formulário de sign up
+  document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+    signUp();
+  });
