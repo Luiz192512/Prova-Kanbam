49952196361fe14e411f59e82dd281a7fd613e68
Luiz192512 <enz.luizgustavo@gmail.com>
2024-04-16 21:53:57 -0300

    Cadastro e Login terminados

--- Kanban 4.0/js/Login.js
+++ Kanban 4.0/js/Login.js
@@ -1,17 +1,14 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
   event.preventDefault();
-
-  const email = document.getElementById('loginEmail').value;
-  const password = document.getElementById('loginPassword').value;
-
-  const storedEmail = localStorage.getItem('userEmail');
-  const storedPassword = localStorage.getItem('userPassword');
-
-  if (email === storedEmail && password === storedPassword) {
-      alert('Login successful!');
-      // Redirect to the dashboard or any other page after successful login
-      window.location.href = 'dashboard.html';
+  var email = localStorage.getItem('email');
+  var password = localStorage.getItem('password');
+  var loginEmail = document.getElementById('loginEmail').value;
+  var loginPassword = document.getElementById('loginPassword').value;
+  if (email === loginEmail && password === loginPassword) {
+    // Authentication successful
+    window.location.href = 'Criar-Kanban.html';
   } else {
-      alert('Invalid email or password');
+    // Authentication failed
+    alert('Invalid email or password');
   }
 });
